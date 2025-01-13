ddf4263b7a542511e3c551a85dc571be3223dcde
kentonvp <kentonvanp@gmail.com>
2025-01-13 08:58:49 -1000

    Holomua on models structure

--- src/database.rs
+++ src/database.rs
@@ -1,29 +1,45 @@
+use anyhow::Result;
 use chrono::NaiveDateTime;
-use homedir::get_my_home;
-use rusqlite::{Connection, Result};
+use expanduser::expanduser;
+use rusqlite::Connection;
 
-pub mod models;
+// Re-exports
+pub use models::Session;
 
-use models::Session;
+mod models {
+    use chrono::NaiveDateTime;
 
-pub fn database_path() -> String {
-    let home = get_my_home()
-        .expect("failed to get home directory")
-        .expect("failed to get home directory");
-    format!(
-        "{}/.config/surflog.db",
-        home.to_str()
-            .expect("failed to convert home directory to string")
-    )
+    #[derive(Debug)]
+    /// Struct representing a surf session.
+    pub struct Session {
+        pub id: Option<u32>,
+        pub location: String,
+        pub date: NaiveDateTime,
+        pub duration: u16,
+        pub rating: u8,
+        pub wave_height: f32,
+    }
 }
 
+/// Get the path to the databaes file: respects the `XDG_DATA_HOME` environment variable if set,
+/// otherwise defaults to `~/.config/surflog.db`.
+pub fn database_path() -> Result<String> {
+    let loc = std::env::var("XDG_DATA_HOME")
+        .unwrap_or(expanduser("~")?.display().to_string());
+    let path = format!("{}/.config/surflog.db", loc);
+    Ok(path)
+}
+
+/// Struct representing a connection to the database.
 pub struct Database {
     conn: Connection,
 }
 
 impl Database {
+    /// Create a new database connection.
     pub fn new(db_path: &str) -> Database {
         let conn = Connection::open(db_path).expect("failed to open database");
+
         conn.execute(
             "CREATE TABLE IF NOT EXISTS sessions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,13 +52,22 @@ impl Database {
             (),
         )
         .expect("failed to create sessions table");
+
         Database { conn }
     }
 
-    pub fn parse_session(row: &rusqlite::Row) -> Result<Session, rusqlite::Error> {
+    /// Helper utility to parse a row from the session table into a `Session` struct.
+    pub fn parse_session(
+        row: &rusqlite::Row,
+    ) -> Result<Session, rusqlite::Error> {
         let date_str: String = row.get(2)?;
-        let date = NaiveDateTime::parse_from_str(&date_str, "%Y-%m-%d %H:%M").map_err(|e| {
-            rusqlite::Error::FromSqlConversionFailure(2, rusqlite::types::Type::Text, Box::new(e))
+        let date = NaiveDateTime::parse_from_str(&date_str, "%Y-%m-%d %H:%M")
+            .map_err(|e| {
+            rusqlite::Error::FromSqlConversionFailure(
+                2,
+                rusqlite::types::Type::Text,
+                Box::new(e),
+            )
         })?;
         Ok(Session {
             id: row.get(0)?,
@@ -54,6 +79,7 @@ impl Database {
         })
     }
 
+    /// Insert a new session into the database session table.
     pub fn insert_session(&self, session: &Session) -> Result<()> {
         let query = "INSERT INTO sessions (location, datetime,duration, rating, wave_height) VALUES (?1, ?2, ?3, ?4, ?5)";
         self.conn
@@ -72,10 +98,14 @@ impl Database {
         Ok(())
     }
 
-    pub fn get_sessions_by_location(&self, location: &str) -> Result<Vec<Session>> {
+    /// Get a session by its ID.
+    pub fn get_sessions_by_location(
+        &self,
+        location: &str,
+    ) -> Result<Vec<Session>> {
         let mut stmt = self
             .conn
-            .prepare("SELECT id, location, datetime, duration, rating, wave_height FROM sessions WHERE location=?1")?;
+            .prepare("SELECT id, location, datetime, duration, rating, wave_height FROM sessions WHERE location=?1 COLLATE NOCASE")?;
         let mut rows = stmt.query_map([location], Self::parse_session)?;
         let mut sessions = Vec::new();
         while let Some(Ok(row)) = rows.next() {
@@ -85,6 +115,7 @@ impl Database {
         Ok(sessions)
     }
 
+    /// Get all sessions in the database.
     pub fn get_sessions(&self) -> Result<Vec<Session>> {
         let mut stmt = self.conn.prepare(
             "SELECT id, location, datetime, duration, rating, wave_height FROM sessions",
@@ -98,6 +129,7 @@ impl Database {
         Ok(sessions)
     }
 
+    /// Delete a session by its ID.
     pub fn delete_session(&self, id: u64) {
         self.conn
             .execute("DELETE FROM sessions WHERE id = ?1", [id])
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
+/*
+ ________       ___  ___      ________      ________      ___           ________      ________      ________
+ |\   ____\     |\  \|\  \    |\   __  \    |\  _____\    |\  \         |\   __  \    |\   ____\    |\   ____\
+ \ \  \___|_    \ \  \\\  \   \ \  \|\  \   \ \  \__/     \ \  \        \ \  \|\  \   \ \  \___|    \ \  \___|_
+ \ \_____  \    \ \  \\\  \   \ \   _  _\   \ \   __\     \ \  \        \ \  \\\  \   \ \  \  ___   \ \_____  \
+  \|____|\  \    \ \  \\\  \   \ \  \\  \|   \ \  \_|      \ \  \____    \ \  \\\  \   \ \  \|\  \   \|____|\  \
+    ____\_\  \    \ \_______\   \ \__\\ _\    \ \__\        \ \_______\   \ \_______\   \ \_______\    ____\_\  \
+   |\_________\    \|_______|    \|__|\|__|    \|__|         \|_______|    \|_______|    \|_______|   |\_________\
+   \|_________|                                                                                       \|_________|
+*/
+
 use chrono::NaiveDateTime;
 
 mod cli;
 mod database;
 
-use database::models;
+use database::{database_path, Database, Session};
 
-fn main() {
-    // Font Name: 3D-ASCII (patrojk.com/software/taag)
-    let app_header = r#"
-        ________       ___  ___      ________      ________      ___           ________      ________      ________
-       |\   ____\     |\  \|\  \    |\   __  \    |\  _____\    |\  \         |\   __  \    |\   ____\    |\   ____\
-       \ \  \___|_    \ \  \\\  \   \ \  \|\  \   \ \  \__/     \ \  \        \ \  \|\  \   \ \  \___|    \ \  \___|_
-        \ \_____  \    \ \  \\\  \   \ \   _  _\   \ \   __\     \ \  \        \ \  \\\  \   \ \  \  ___   \ \_____  \
-         \|____|\  \    \ \  \\\  \   \ \  \\  \|   \ \  \_|      \ \  \____    \ \  \\\  \   \ \  \|\  \   \|____|\  \
-           ____\_\  \    \ \_______\   \ \__\\ _\    \ \__\        \ \_______\   \ \_______\   \ \_______\    ____\_\  \
-          |\_________\    \|_______|    \|__|\|__|    \|__|         \|_______|    \|_______|    \|_______|   |\_________\
-          \|_________|                                                                                       \|_________|
-    "#;
-    print!("{app_header}");
+use anyhow::Result;
+use log::debug;
+
+fn main() -> Result<()> {
     let args = cli::get_args();
 
-    let db = database::Database::new(&database::database_path());
+    let db = Database::new(&database_path()?);
 
     match args.cmd {
         cli::Command::Add(add_opts) => {
-            println!("Adding a new session...");
-            let session = models::Session {
+            debug!("Adding a new session...");
+            let session = Session {
                 id: None,
                 location: add_opts.location,
-                date: NaiveDateTime::parse_from_str(&add_opts.datetime, "%Y%m%d%H%M").unwrap(),
+                date: NaiveDateTime::parse_from_str(
+                    &add_opts.datetime,
+                    "%Y-%m-%d %H:%M",
+                )
+                .unwrap(),
                 duration: add_opts.duration,
                 rating: add_opts.rating,
                 wave_height: add_opts.wave_height,
@@ -38,11 +44,11 @@ fn main() {
             let _ = db.insert_session(&session);
         }
         cli::Command::Delete(del_opts) => {
-            println!("Delete a session...");
+            debug!("Delete a session...");
             db.delete_session(del_opts.id);
         }
         cli::Command::List(list_opts) => {
-            println!("Listing all sessions...");
+            debug!("Listing all sessions...");
             // Display all sessions
             let sessions = match list_opts.location {
                 Some(loc) => db.get_sessions_by_location(&loc),
@@ -50,18 +56,24 @@ fn main() {
             };
 
             if let Ok(sessions) = sessions {
-                println!("{:-<80}", "");
+                //println!("{:-<80}", "");
                 println!(
                     "|{:^4}|{:^20}|{:^18}|{:^10}|{:^8}|{:^13}|",
-                    "Id", "Location", "DateTime", "Duration", "Rating", "Wave Height"
+                    "Id",
+                    "Location",
+                    "DateTime",
+                    "Duration",
+                    "Rating",
+                    "Wave Height"
                 );
                 println!("{:-<80}", "");
                 for session in sessions {
                     println!(
                         "|{:^4}|{:^20}|{:^18}|{:^10}|{:^8}|{:^13}|",
-                        session
-                            .id
-                            .map_or_else(|| String::from("None"), |x| x.to_string()),
+                        session.id.map_or_else(
+                            || String::from("None"),
+                            |x| x.to_string()
+                        ),
                         session.location,
                         session.date.format("%Y-%m-%d %H:%M"),
                         format!("{} min", session.duration),
@@ -69,12 +81,13 @@ fn main() {
                         format!("{}", session.wave_height),
                     );
                 }
-                println!("{:-<80}", "");
+                //println!("{:-<80}", "");
             }
         }
         cli::Command::Config => {
-            println!("Showing configuration...");
-            println!("db_path = {}", database::database_path());
+            debug!("Showing configuration...");
+            println!("db_path = {}", database_path()?);
         }
     }
+    Ok(())
 }
